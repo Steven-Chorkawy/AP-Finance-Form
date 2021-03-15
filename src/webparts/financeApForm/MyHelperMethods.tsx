dbbc792e0af585ca67b0b0faa1bf832f34b99556
Admin <scadmin@clarington.net>
2021-03-15 11:48:11 -0400

    Adding methods to get user data.

--- src/webparts/financeApForm/MyHelperMethods.tsx
+++ src/webparts/financeApForm/MyHelperMethods.tsx
@@ -1,3 +1,5 @@
+import { sp } from "@pnp/sp";
+
 
 export const FormatCurrency = (n: number): string => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(n);
@@ -9,3 +11,16 @@ export const SumAccounts = (accounts): string => {
     }
     return FormatCurrency(accounts.reduce((a, b) => a + (b['AmountIncludingTaxes'] || 0), 0));
 };
+
+export const GetUserByLoginName = async (loginName: string): Promise<any> => {
+    return await sp.web.siteUsers.getByLoginName(loginName).get();
+  };
+  
+  export const GetUsersByLoginName = async (users: Array<any>): Promise<Array<any>> => {
+    let returnOutput: Array<any> = [];
+    for (let index = 0; index < users.length; index++) {
+      const user = users[index];
+      returnOutput.push(await GetUserByLoginName(user.loginName));
+    }
+    return returnOutput;
+  };
