ea94f094d82e15b35fa7cca7805cd599c7d24f45
Steven Chorkawy <schorkawy@clarington.net>
2022-06-28 11:47:20 -0400

    Removing extra tabs.

--- src/webparts/financeApForm/MyHelperMethods.tsx
+++ src/webparts/financeApForm/MyHelperMethods.tsx
@@ -2,25 +2,25 @@ import { sp } from "@pnp/sp";
 
 
 export const FormatCurrency = (n: number): string => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(n);
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(n);
 };
 
 export const SumAccounts = (accounts): string => {
-    if (!accounts) {
-        return FormatCurrency(0);
-    }
-    return FormatCurrency(accounts.reduce((a, b) => a + (b['AmountIncludingTaxes'] || 0), 0));
+  if (!accounts) {
+    return FormatCurrency(0);
+  }
+  return FormatCurrency(accounts.reduce((a, b) => a + (b['AmountIncludingTaxes'] || 0), 0));
 };
 
 export const GetUserByLoginName = async (loginName: string): Promise<any> => {
-    return await sp.web.siteUsers.getByLoginName(loginName).get();
-  };
-  
-  export const GetUsersByLoginName = async (users: Array<any>): Promise<Array<any>> => {
-    let returnOutput: Array<any> = [];
-    for (let index = 0; index < users.length; index++) {
-      const user = users[index];
-      returnOutput.push(await GetUserByLoginName(user.loginName));
-    }
-    return returnOutput;
-  };
+  return await sp.web.siteUsers.getByLoginName(loginName).get();
+};
+
+export const GetUsersByLoginName = async (users: Array<any>): Promise<Array<any>> => {
+  let returnOutput: Array<any> = [];
+  for (let index = 0; index < users.length; index++) {
+    const user = users[index];
+    returnOutput.push(await GetUserByLoginName(user.loginName));
+  }
+  return returnOutput;
+};
