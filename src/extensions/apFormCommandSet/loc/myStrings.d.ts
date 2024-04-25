1b4288029c57ea37220f040fab4dfd9c21198a66
Steven Chorkawy <schorkawy@clarington.net>
2024-04-25 13:27:40 -0400

    scaffolding a command set button.

---
+++ src/extensions/apFormCommandSet/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IApFormCommandSetCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'ApFormCommandSetCommandSetStrings' {
+  const strings: IApFormCommandSetCommandSetStrings;
+  export = strings;
+}
